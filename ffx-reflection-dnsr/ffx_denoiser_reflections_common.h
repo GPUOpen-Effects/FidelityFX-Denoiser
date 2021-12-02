d7dfecbabe7b9523b14e7b067216e06b86e8d189
Rys Sommefeldt <rys.sommefeldt@amd.com>
2021-12-02 11:52:20 +0000

    FidelityFX Denoiser v1.2

--- ffx-reflection-dnsr/ffx_denoiser_reflections_common.h
+++ ffx-reflection-dnsr/ffx_denoiser_reflections_common.h
@@ -23,79 +23,121 @@ THE SOFTWARE.
 #ifndef FFX_DNSR_REFLECTIONS_COMMON
 #define FFX_DNSR_REFLECTIONS_COMMON
 
-uint FFX_DNSR_Reflections_RoundedDivide(uint value, uint divisor) {
-    return (value + divisor - 1) / divisor;
-}
-
-uint FFX_DNSR_Reflections_GetTileMetaDataIndex(uint2 pixel_pos, uint screen_width) {
-    uint2 tile_index = uint2(pixel_pos.x / 8, pixel_pos.y / 8);
-    uint flattened = tile_index.y * FFX_DNSR_Reflections_RoundedDivide(screen_width, 8) + tile_index.x;
-    return flattened;
-}
-
-uint FFX_DNSR_Reflections_GetTemporalVarianceIndex(uint2 pixel_pos, uint screen_width) {
-    uint2 tile_index = uint2(pixel_pos.x / 8, pixel_pos.y / 8);
-    uint flattened = tile_index.y * FFX_DNSR_Reflections_RoundedDivide(screen_width, 8) + tile_index.x;
-    return 2 * flattened + ((pixel_pos.y % 8) / 4); // Position upper and lower half next to each other
-}
-
-uint FFX_DNSR_Reflections_GetBitMaskFromPixelPosition(uint2 pixel_pos) {
-    int lane_index = (pixel_pos.y % 4) * 8 + (pixel_pos.x % 8);
-    return (1u << lane_index);
-}
-
-float FFX_DNSR_Reflections_GetEdgeStoppingNormalWeight(float3 normal_p, float3 normal_q, float sigma) {
-    return pow(max(dot(normal_p, normal_q), 0.0), sigma);
-}
-
-float FFX_DNSR_Reflections_GetEdgeStoppingRoughnessWeight(float roughness_p, float roughness_q, float sigma_min, float sigma_max) {
-    return 1.0 - smoothstep(sigma_min, sigma_max, abs(roughness_p - roughness_q));
-}
-
-min16float FFX_DNSR_Reflections_GetEdgeStoppingRoughnessWeightFP16(min16float roughness_p, min16float roughness_q, min16float sigma_min, min16float sigma_max) {
-    return 1.0 - smoothstep(sigma_min, sigma_max, abs(roughness_p - roughness_q));
-}
-
-// Roughness weight to prevent ghosting on pure mirror reflections
-float FFX_DNSR_Reflections_GetRoughnessAccumulationWeight(float roughness) {
-    float near_singular_roughness = 0.00001;
-    return smoothstep(0.0, near_singular_roughness, roughness);
-}
-
-float FFX_DNSR_Reflections_Gaussian(float x, float m, float sigma) {
-    float a = length(x - m) / sigma;
-    a *= a;
-    return exp(-0.5 * a);
-}
-
-float FFX_DNSR_Reflections_Luminance(float3 color) {
-    return max(dot(color, float3(0.299, 0.587, 0.114)), 0.00001);
-}
+#include "ffx_denoiser_reflections_config.h"
 
 uint FFX_DNSR_Reflections_BitfieldExtract(uint src, uint off, uint bits) {
-    uint mask = (1 << bits) - 1; 
-    return (src >> off) & mask; 
+    uint mask = (1 << bits) - 1;
+    return (src >> off) & mask;
 }
 
-uint FFX_DNSR_Reflections_BitfieldInsert(uint src, uint ins, uint bits) { 
-    uint mask = (1 << bits) - 1; 
-    return (ins & mask) | (src & (~mask)); 
+uint FFX_DNSR_Reflections_BitfieldInsert(uint src, uint ins, uint bits) {
+    uint mask = (1 << bits) - 1;
+    return (ins & mask) | (src & (~mask));
 }
 
 //  LANE TO 8x8 MAPPING
 //  ===================
-//  00 01 08 09 10 11 18 19 
+//  00 01 08 09 10 11 18 19
 //  02 03 0a 0b 12 13 1a 1b
 //  04 05 0c 0d 14 15 1c 1d
-//  06 07 0e 0f 16 17 1e 1f 
-//  20 21 28 29 30 31 38 39 
+//  06 07 0e 0f 16 17 1e 1f
+//  20 21 28 29 30 31 38 39
 //  22 23 2a 2b 32 33 3a 3b
 //  24 25 2c 2d 34 35 3c 3d
-//  26 27 2e 2f 36 37 3e 3f 
-uint2 FFX_DNSR_Reflections_RemapLane8x8(uint lane) { 
-    return uint2(FFX_DNSR_Reflections_BitfieldInsert(FFX_DNSR_Reflections_BitfieldExtract(lane, 2u, 3u), lane, 1u)
-    , FFX_DNSR_Reflections_BitfieldInsert(FFX_DNSR_Reflections_BitfieldExtract(lane, 3u, 3u)
-    , FFX_DNSR_Reflections_BitfieldExtract(lane, 1u, 2u), 2u));
+//  26 27 2e 2f 36 37 3e 3f
+uint2 FFX_DNSR_Reflections_RemapLane8x8(uint lane) {
+    return uint2(FFX_DNSR_Reflections_BitfieldInsert(FFX_DNSR_Reflections_BitfieldExtract(lane, 2u, 3u), lane, 1u),
+                 FFX_DNSR_Reflections_BitfieldInsert(FFX_DNSR_Reflections_BitfieldExtract(lane, 3u, 3u), FFX_DNSR_Reflections_BitfieldExtract(lane, 1u, 2u), 2u));
 }
+
+min16float FFX_DNSR_Reflections_Luminance(min16float3 color) { return max(dot(color, float3(0.299, 0.587, 0.114)), 0.001); }
+
+min16float FFX_DNSR_Reflections_ComputeTemporalVariance(min16float3 history_radiance, min16float3 radiance) {
+    min16float history_luminance = FFX_DNSR_Reflections_Luminance(history_radiance);
+    min16float luminance         = FFX_DNSR_Reflections_Luminance(radiance);
+    min16float diff              = abs(history_luminance - luminance) / max(max(history_luminance, luminance), 0.5);
+    return diff * diff;
+}
+
+uint FFX_DNSR_Reflections_PackFloat16(min16float2 v) {
+    uint2 p = f32tof16(float2(v));
+    return p.x | (p.y << 16);
+}
+
+min16float2 FFX_DNSR_Reflections_UnpackFloat16(uint a) {
+    float2 tmp = f16tof32(uint2(a & 0xFFFF, a >> 16));
+    return min16float2(tmp);
+}
+
+uint2 FFX_DNSR_Reflections_PackFloat16_4(min16float4 v) { return uint2(FFX_DNSR_Reflections_PackFloat16(v.xy), FFX_DNSR_Reflections_PackFloat16(v.zw)); }
+
+min16float4 FFX_DNSR_Reflections_UnpackFloat16_4(uint2 a) { return min16float4(FFX_DNSR_Reflections_UnpackFloat16(a.x), FFX_DNSR_Reflections_UnpackFloat16(a.y)); }
+
+// Rounds value to the nearest multiple of 8
+uint2 FFX_DNSR_Reflections_RoundUp8(uint2 value) {
+    uint2 round_down = value & ~0b111;
+    return (round_down == value) ? value : value + 8;
+}
+
+// From "Temporal Reprojection Anti-Aliasing"
+// https://github.com/playdeadgames/temporal
+/**********************************************************************
+Copyright (c) [2015] [Playdead]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+********************************************************************/
+min16float3 FFX_DNSR_Reflections_ClipAABB(min16float3 aabb_min, min16float3 aabb_max, min16float3 prev_sample) {
+    // Main idea behind clipping - it prevents clustering when neighbor color space
+    // is distant from history sample
+
+    // Here we find intersection between color vector and aabb color box
+
+    // Note: only clips towards aabb center
+    float3 aabb_center = 0.5 * (aabb_max + aabb_min);
+    float3 extent_clip = 0.5 * (aabb_max - aabb_min) + 0.001;
+
+    // Find color vector
+    float3 color_vector = prev_sample - aabb_center;
+    // Transform into clip space
+    float3 color_vector_clip = color_vector / extent_clip;
+    // Find max absolute component
+    color_vector_clip       = abs(color_vector_clip);
+    min16float max_abs_unit = max(max(color_vector_clip.x, color_vector_clip.y), color_vector_clip.z);
+
+    if (max_abs_unit > 1.0) {
+        return aabb_center + color_vector / max_abs_unit; // clip towards color vector
+    } else {
+        return prev_sample; // point is inside aabb
+    }
+}
+
+#ifdef FFX_DNSR_REFLECTIONS_ESTIMATES_LOCAL_NEIGHBORHOOD
+
+#    ifndef FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS
+#        define FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS 4
+#    endif
+
+min16float FFX_DNSR_Reflections_LocalNeighborhoodKernelWeight(min16float i) {
+    const min16float radius = FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS + 1.0;
+    return exp(-FFX_DNSR_REFLECTIONS_GAUSSIAN_K * (i * i) / (radius * radius));
+}
+
+#endif // FFX_DNSR_REFLECTIONS_ESTIMATES_LOCAL_NEIGHBORHOOD
 
 #endif // FFX_DNSR_REFLECTIONS_COMMON
--- ffx-reflection-dnsr/ffx_denoiser_reflections_resolve_temporal.h
+++ ffx-reflection-dnsr/ffx_denoiser_reflections_resolve_temporal.h
@@ -23,223 +23,124 @@ THE SOFTWARE.
 #ifndef FFX_DNSR_REFLECTIONS_RESOLVE_TEMPORAL
 #define FFX_DNSR_REFLECTIONS_RESOLVE_TEMPORAL
 
+#define FFX_DNSR_REFLECTIONS_ESTIMATES_LOCAL_NEIGHBORHOOD
 #include "ffx_denoiser_reflections_common.h"
 
-// From "Temporal Reprojection Anti-Aliasing"
-// https://github.com/playdeadgames/temporal
-/**********************************************************************
-Copyright (c) [2015] [Playdead]
+groupshared uint g_ffx_dnsr_shared_0[16][16];
+groupshared uint g_ffx_dnsr_shared_1[16][16];
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+struct FFX_DNSR_Reflections_NeighborhoodSample {
+    min16float3 radiance;
+};
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+FFX_DNSR_Reflections_NeighborhoodSample FFX_DNSR_Reflections_LoadFromGroupSharedMemory(int2 idx) {
+    uint2       packed_radiance   = uint2(g_ffx_dnsr_shared_0[idx.y][idx.x], g_ffx_dnsr_shared_1[idx.y][idx.x]);
+    min16float3 unpacked_radiance = FFX_DNSR_Reflections_UnpackFloat16_4(packed_radiance).xyz;
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-********************************************************************/
-float3 FFX_DNSR_Reflections_ClipAABB(float3 aabb_min, float3 aabb_max, float3 prev_sample) {
-    // Main idea behind clipping - it prevents clustering when neighbor color space
-    // is distant from history sample
-
-    // Here we find intersection between color vector and aabb color box
-
-    // Note: only clips towards aabb center
-    float3 aabb_center = 0.5 * (aabb_max + aabb_min);
-    float3 extent_clip = 0.5 * (aabb_max - aabb_min) + 0.001;
-
-    // Find color vector
-    float3 color_vector = prev_sample - aabb_center;
-    // Transform into clip space
-    float3 color_vector_clip = color_vector / extent_clip;
-    // Find max absolute component
-    color_vector_clip = abs(color_vector_clip);
-    float max_abs_unit = max(max(color_vector_clip.x, color_vector_clip.y), color_vector_clip.z);
-
-    if (max_abs_unit > 1.0) {
-        return aabb_center + color_vector / max_abs_unit; // clip towards color vector
-    }
-    else {
-        return prev_sample; // point is inside aabb
-    }
+    FFX_DNSR_Reflections_NeighborhoodSample sample;
+    sample.radiance = unpacked_radiance;
+    return sample;
 }
 
-// Estimates spatial reflection radiance standard deviation
-float3 FFX_DNSR_Reflections_EstimateLocalNeighborhood(int2 dispatch_thread_id) {
-    float3 color_sum = 0.0;
-    float3 color_sum_squared = 0.0;
+struct FFX_DNSR_Reflections_Moments {
+    min16float3 mean;
+    min16float3 variance;
+};
 
-    int radius = 1;
-    float weight = (radius * 2.0 + 1.0) * (radius * 2.0 + 1.0);
-
-    for (int dx = -radius; dx <= radius; dx++) {
-        for (int dy = -radius; dy <= radius; dy++) {
-            int2 texel_coords = dispatch_thread_id + int2(dx, dy);
-            float3 value = FFX_DNSR_Reflections_LoadSpatiallyDenoisedReflections(texel_coords);
-            color_sum += value;
-            color_sum_squared += value * value;
+FFX_DNSR_Reflections_Moments FFX_DNSR_Reflections_EstimateLocalNeighborhoodInGroup(int2 group_thread_id) {
+    FFX_DNSR_Reflections_Moments estimate;
+    estimate.mean                 = 0;
+    estimate.variance             = 0;
+    min16float accumulated_weight = 0;
+    for (int j = -FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS; j <= FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS; ++j) {
+        for (int i = -FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS; i <= FFX_DNSR_REFLECTIONS_LOCAL_NEIGHBORHOOD_RADIUS; ++i) {
+            int2        new_idx  = group_thread_id + int2(i, j);
+            min16float3 radiance = FFX_DNSR_Reflections_LoadFromGroupSharedMemory(new_idx).radiance;
+            min16float  weight   = FFX_DNSR_Reflections_LocalNeighborhoodKernelWeight(i) * FFX_DNSR_Reflections_LocalNeighborhoodKernelWeight(j);
+            accumulated_weight  += weight;
+            estimate.mean       += radiance * weight;
+            estimate.variance   += radiance * radiance * weight;
         }
     }
+    estimate.mean     /= accumulated_weight;
+    estimate.variance /= accumulated_weight;
 
-    float3 color_std = (color_sum_squared - color_sum * color_sum / weight) / (weight - 1.0);
-    return sqrt(max(color_std, 0.0));
+    estimate.variance = abs(estimate.variance - estimate.mean * estimate.mean);
+    return estimate;
 }
 
-float2 FFX_DNSR_Reflections_GetSurfaceReprojection(int2 dispatch_thread_id, float2 uv, float2 motion_vector) {
-    // Reflector position reprojection
-    float2 history_uv = uv - motion_vector;
-    return history_uv;
+void FFX_DNSR_Reflections_StoreInGroupSharedMemory(int2 group_thread_id, min16float3 radiance) {
+    g_ffx_dnsr_shared_0[group_thread_id.y][group_thread_id.x] = FFX_DNSR_Reflections_PackFloat16(radiance.xy);
+    g_ffx_dnsr_shared_1[group_thread_id.y][group_thread_id.x] = FFX_DNSR_Reflections_PackFloat16(radiance.zz);
 }
 
-float2 FFX_DNSR_Reflections_GetHitPositionReprojection(int2 dispatch_thread_id, float2 uv, float reflected_ray_length) {
-    float z = FFX_DNSR_Reflections_LoadDepth(dispatch_thread_id);
-    float3 view_space_ray = FFX_DNSR_Reflections_ScreenSpaceToViewSpace(float3(uv, z));
+void FFX_DNSR_Reflections_LoadNeighborhood(int2 pixel_coordinate, out min16float3 radiance) { radiance = FFX_DNSR_Reflections_LoadRadiance(pixel_coordinate); }
 
-    // We start out with reconstructing the ray length in view space. 
-    // This includes the portion from the camera to the reflecting surface as well as the portion from the surface to the hit position.
-    float surface_depth = length(view_space_ray);
-    float ray_length = surface_depth + reflected_ray_length;
+void FFX_DNSR_Reflections_InitializeGroupSharedMemory(int2 dispatch_thread_id, int2 group_thread_id, int2 screen_size) {
+    // Load 16x16 region into shared memory using 4 8x8 blocks.
+    int2 offset[4] = {int2(0, 0), int2(8, 0), int2(0, 8), int2(8, 8)};
 
-    // We then perform a parallax correction by shooting a ray 
-    // of the same length "straight through" the reflecting surface
-    // and reprojecting the tip of that ray to the previous frame.
-    view_space_ray /= surface_depth; // == normalize(view_space_ray)
-    view_space_ray *= ray_length;
-    float3 world_hit_position = FFX_DNSR_Reflections_ViewSpaceToWorldSpace(float4(view_space_ray, 1)); // This is the "fake" hit position if we would follow the ray straight through the surface.
-    float3 prev_hit_position = FFX_DNSR_Reflections_WorldSpaceToScreenSpacePrevious(world_hit_position);
-    float2 history_uv = prev_hit_position.xy;
-    return history_uv;
-}
+    // Intermediate storage registers to cache the result of all loads
+    min16float3 radiance[4];
 
-float FFX_DNSR_Reflections_SampleHistory(float2 uv, uint2 screen_size, float3 normal, float roughness, float3 radiance_min, float3 radiance_max, float roughness_sigma_min, float roughness_sigma_max,  float temporalStabilityFactor, out float3 radiance) {
-    int2 texel_coords = int2(screen_size * uv);
-    radiance = FFX_DNSR_Reflections_LoadRadianceHistory(texel_coords);
-    radiance = FFX_DNSR_Reflections_ClipAABB(radiance_min, radiance_max, radiance);
+    // Start in the upper left corner of the 16x16 region.
+    dispatch_thread_id -= 4;
 
-    float3 history_normal =   FFX_DNSR_Reflections_LoadNormalHistory(texel_coords);
-    float history_roughness = FFX_DNSR_Reflections_LoadRoughnessHistory(texel_coords);
+    // First store all loads in registers
+    for (int i = 0; i < 4; ++i) {
+        FFX_DNSR_Reflections_LoadNeighborhood(dispatch_thread_id + offset[i], radiance[i]);
+    }
 
-    const float normal_sigma = 8.0;
-
-    float accumulation_speed = temporalStabilityFactor
-        * FFX_DNSR_Reflections_GetEdgeStoppingNormalWeight(normal, history_normal, normal_sigma)
-        * FFX_DNSR_Reflections_GetEdgeStoppingRoughnessWeight(roughness, history_roughness, roughness_sigma_min, roughness_sigma_max)
-        * FFX_DNSR_Reflections_GetRoughnessAccumulationWeight(roughness)
-        ;
-
-    return saturate(accumulation_speed);
-}
-
-float FFX_DNSR_Reflections_ComputeTemporalVariance(float3 history_radiance, float3 radiance) {
-    // Check temporal variance. 
-    float history_luminance = FFX_DNSR_Reflections_Luminance(history_radiance);
-    float luminance = FFX_DNSR_Reflections_Luminance(radiance);
-    return abs(history_luminance - luminance) / max(max(history_luminance, luminance), 0.00001);
-}
-
-groupshared uint g_FFX_DNSR_Reflections_TemporalVarianceMask[2];
-
-void FFX_DNSR_Reflections_WriteTemporalVarianceMask(uint mask_write_index, uint has_temporal_variance_mask) {
-    // All lanes write to the same index, so we combine the masks within the wave and do a single write
-    const uint s_has_temporal_variance_mask = WaveActiveBitOr(has_temporal_variance_mask);
-    if (WaveIsFirstLane()) {
-        FFX_DNSR_Reflections_StoreTemporalVarianceMask(mask_write_index, s_has_temporal_variance_mask);
+    // Then move all registers to groupshared memory
+    for (int j = 0; j < 4; ++j) {
+        FFX_DNSR_Reflections_StoreInGroupSharedMemory(group_thread_id + offset[j], radiance[j]);
     }
 }
 
-void FFX_DNSR_Reflections_WriteTemporalVariance(int2 dispatch_thread_id, int2 group_thread_id, uint2 screen_size, bool has_temporal_variance) {
-    uint mask_write_index = FFX_DNSR_Reflections_GetTemporalVarianceIndex(dispatch_thread_id, screen_size.x);
-    uint lane_mask = FFX_DNSR_Reflections_GetBitMaskFromPixelPosition(dispatch_thread_id);
-    uint has_temporal_variance_mask = has_temporal_variance ? lane_mask : 0;
+void FFX_DNSR_Reflections_ResolveTemporal(int2 dispatch_thread_id, int2 group_thread_id, uint2 screen_size, float2 inv_screen_size, float history_clip_weight) {
+    FFX_DNSR_Reflections_InitializeGroupSharedMemory(dispatch_thread_id, group_thread_id, screen_size);
+    GroupMemoryBarrierWithGroupSync();
 
-    if (WaveGetLaneCount() == 32) {
-        FFX_DNSR_Reflections_WriteTemporalVarianceMask(mask_write_index, has_temporal_variance_mask);
-    }
-    else if (WaveGetLaneCount() == 64) { // The lower 32 lanes write to a different index than the upper 32 lanes.
-        if (WaveGetLaneIndex() < 32) {
-            FFX_DNSR_Reflections_WriteTemporalVarianceMask(mask_write_index, has_temporal_variance_mask); // Write lower
-        }
-        else {
-            FFX_DNSR_Reflections_WriteTemporalVarianceMask(mask_write_index, has_temporal_variance_mask); // Write upper
-        }
-    }
-    else { // Use groupshared memory for all other wave sizes
-        uint mask_index = group_thread_id.y / 4;
-        g_FFX_DNSR_Reflections_TemporalVarianceMask[mask_index] = 0;
-        GroupMemoryBarrierWithGroupSync();
-        InterlockedOr(g_FFX_DNSR_Reflections_TemporalVarianceMask[mask_index], has_temporal_variance_mask);
-        GroupMemoryBarrierWithGroupSync();
+    group_thread_id += 4; // Center threads in groupshared memory
 
-        if (all(group_thread_id == 0)) {
-            FFX_DNSR_Reflections_StoreTemporalVarianceMask(mask_write_index, g_FFX_DNSR_Reflections_TemporalVarianceMask[0]);
-            FFX_DNSR_Reflections_StoreTemporalVarianceMask(mask_write_index + 1, g_FFX_DNSR_Reflections_TemporalVarianceMask[1]);
+    FFX_DNSR_Reflections_NeighborhoodSample center       = FFX_DNSR_Reflections_LoadFromGroupSharedMemory(group_thread_id);
+    min16float3                             new_signal   = center.radiance;
+    min16float                              roughness    = FFX_DNSR_Reflections_LoadRoughness(dispatch_thread_id);
+    min16float                              new_variance = FFX_DNSR_Reflections_LoadVariance(dispatch_thread_id);
+    
+    if (FFX_DNSR_Reflections_IsGlossyReflection(roughness)) {
+        min16float  num_samples  = FFX_DNSR_Reflections_LoadNumSamples(dispatch_thread_id);
+        float2      uv8          = (float2(dispatch_thread_id.xy) + (0.5).xx) / FFX_DNSR_Reflections_RoundUp8(screen_size);
+        min16float3 avg_radiance = FFX_DNSR_Reflections_SampleAverageRadiance(uv8);
+
+        min16float3                  old_signal         = FFX_DNSR_Reflections_LoadRadianceReprojected(dispatch_thread_id);
+        FFX_DNSR_Reflections_Moments local_neighborhood = FFX_DNSR_Reflections_EstimateLocalNeighborhoodInGroup(group_thread_id);
+        // Clip history based on the curren local statistics
+        min16float3                  color_std          = (sqrt(local_neighborhood.variance.xyz) + length(local_neighborhood.mean.xyz - avg_radiance)) * history_clip_weight * 1.4;
+                            local_neighborhood.mean.xyz = lerp(local_neighborhood.mean.xyz, avg_radiance, 0.2);
+        min16float3                  radiance_min       = local_neighborhood.mean.xyz - color_std;
+        min16float3                  radiance_max       = local_neighborhood.mean.xyz + color_std;
+        min16float3                  clipped_old_signal = FFX_DNSR_Reflections_ClipAABB(radiance_min, radiance_max, old_signal.xyz);
+        min16float                   accumulation_speed = 1.0 / max(num_samples, 1.0);
+        min16float                   weight             = (1.0 - accumulation_speed);
+        // Blend with average for small sample count
+        new_signal.xyz                                  = lerp(new_signal.xyz, avg_radiance, 1.0 / max(num_samples + 1.0f, 1.0));
+        // Clip outliers
+        {
+            min16float3                  radiance_min       = avg_radiance.xyz - color_std * 1.0;
+            min16float3                  radiance_max       = avg_radiance.xyz + color_std * 1.0;
+            new_signal.xyz                                  = FFX_DNSR_Reflections_ClipAABB(radiance_min, radiance_max, new_signal.xyz);
         }
+        // Blend with history
+        new_signal                                      = lerp(new_signal, clipped_old_signal, weight);
+        new_variance                                    = lerp(FFX_DNSR_Reflections_ComputeTemporalVariance(new_signal.xyz, clipped_old_signal.xyz), new_variance, weight);
+        if (any(isinf(new_signal)) || any(isnan(new_signal)) || any(isinf(new_variance)) || any(isnan(new_variance))) {
+            new_signal   = 0.0;
+            new_variance = 0.0;
+        }
+
     }
+    FFX_DNSR_Reflections_StoreTemporalAccumulation(dispatch_thread_id, new_signal, new_variance);
 }
 
-void FFX_DNSR_Reflections_ResolveTemporal(int2 dispatch_thread_id, int2 group_thread_id, uint2 screen_size, float temporal_stability_factor, float temporal_variance_threshold) {
-
-    // First check if we have to denoise or if a simple copy is enough
-    uint tile_meta_data_index = FFX_DNSR_Reflections_GetTileMetaDataIndex(dispatch_thread_id, screen_size.x);
-    tile_meta_data_index = WaveReadLaneFirst(tile_meta_data_index);
-    bool needs_denoiser = FFX_DNSR_Reflections_LoadTileMetaDataMask(tile_meta_data_index);
-
-    bool has_temporal_variance = false;
-
-    [branch]
-    if (needs_denoiser) {
-        float roughness = FFX_DNSR_Reflections_LoadRoughness(dispatch_thread_id);
-        if (!FFX_DNSR_Reflections_IsGlossyReflection(roughness) || FFX_DNSR_Reflections_IsMirrorReflection(roughness)) {
-            return;
-        }
-
-        float2 uv = float2(dispatch_thread_id.x + 0.5, dispatch_thread_id.y + 0.5) / screen_size;
-
-        float3 normal = FFX_DNSR_Reflections_LoadNormal(dispatch_thread_id);
-        float3 radiance = FFX_DNSR_Reflections_LoadSpatiallyDenoisedReflections(dispatch_thread_id);
-        float3 radiance_history = FFX_DNSR_Reflections_LoadRadianceHistory(dispatch_thread_id);
-        float ray_length = FFX_DNSR_Reflections_LoadRayLength(dispatch_thread_id);
-
-        // And clip it to the local neighborhood
-        float2 motion_vector = FFX_DNSR_Reflections_LoadMotionVector(dispatch_thread_id);
-        float3 color_std = FFX_DNSR_Reflections_EstimateLocalNeighborhood(dispatch_thread_id);
-        color_std *= 2.2;
-
-        float3 radiance_min = radiance.xyz - color_std;
-        float3 radiance_max = radiance + color_std;
-
-        // Reproject point on the reflecting surface
-        float2 surface_reprojection_uv = FFX_DNSR_Reflections_GetSurfaceReprojection(dispatch_thread_id, uv, motion_vector);
-
-        // Reproject hit point
-        float2 hit_reprojection_uv = FFX_DNSR_Reflections_GetHitPositionReprojection(dispatch_thread_id, uv, ray_length);
-
-        float2 reprojection_uv;
-        reprojection_uv = (roughness < 0.05) ? hit_reprojection_uv : surface_reprojection_uv;
-
-        float3 reprojection = 0;
-        float weight = 0;
-        if (all(reprojection_uv > 0.0) && all(reprojection_uv < 1.0)) {
-            weight = FFX_DNSR_Reflections_SampleHistory(reprojection_uv, screen_size, normal, roughness, radiance_min, radiance_max, g_roughness_sigma_min, g_roughness_sigma_max, temporal_stability_factor, reprojection);
-        }
-
-        radiance = lerp(radiance, reprojection, weight);
-        has_temporal_variance = FFX_DNSR_Reflections_ComputeTemporalVariance(radiance_history, radiance) > temporal_variance_threshold;
-
-        FFX_DNSR_Reflections_StoreTemporallyDenoisedReflections(dispatch_thread_id, radiance);
-    }
-
-    FFX_DNSR_Reflections_WriteTemporalVariance(dispatch_thread_id, group_thread_id, screen_size, has_temporal_variance);
-}
-
-#endif //FFX_DNSR_REFLECTIONS_RESOLVE_TEMPORAL
+#endif // FFX_DNSR_REFLECTIONS_RESOLVE_TEMPORAL
